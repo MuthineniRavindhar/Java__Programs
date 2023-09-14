95ceca0b77fc53dd37acb256875ffc7b0f367ab8
MuthineniRavindhar <139392018+MuthineniRavindhar@users.noreply.github.com>
2023-09-14 22:22:37 +0530

    Create ReverseString.java

---
+++ String Programs"/ReverseString.java
@@ -0,0 +1,15 @@
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		String str="developer";
+		String s1="";
+		for(int i=0; i<str.length(); i++) {
+			char ch=str.charAt(i);
+			s1=ch+s1;
+			
+//			System.out.println(s1);  //to check the iterations.
+		}
+		System.out.println(s1);
+	}
+}
