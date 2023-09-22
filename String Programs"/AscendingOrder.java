e686c6c28ab4db0799639f9e78fdcf0789689ea1
MuthineniRavindhar <139392018+MuthineniRavindhar@users.noreply.github.com>
2023-09-22 15:47:59 +0530

    Create AscendingOrder.java

---
+++ String Programs"/AscendingOrder.java
@@ -0,0 +1,30 @@
+package strings;
+
+import java.util.Iterator;
+
+public class AscendingOrder {
+
+	public static void main(String[] args) {
+		
+		String s="dacbfe";
+		char[] arr=s.toCharArray();
+		
+		for(int i=0; i<arr.length; i++) {
+			int min=i;
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[j]<arr[min]) {
+					min=j;
+					
+				}
+				char temp = arr[i];
+				arr[i]=arr[min];
+				arr[min]=temp;
+			}
+			
+		}
+		System.out.print("The Ascending Order is : ");
+		for (char c : arr) {
+			System.out.print(c);
+		}
+	}
+}
