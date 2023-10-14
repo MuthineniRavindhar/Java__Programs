bb5e64e331e6d5a7206be13543cfcc49672ff272
MuthineniRavindhar <139392018+MuthineniRavindhar@users.noreply.github.com>
2023-10-14 09:42:34 +0530

    Create Swapping.java

---
+++ Number Programs/Swapping.java
@@ -0,0 +1,25 @@
+package numberprograms;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c;
+		
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+		
+//		or
+		
+//		a=a+b;
+//		b=a-b;
+//		a=a-b;
+//		System.out.println(a);
+//		System.out.println(b);
+	}
+}
