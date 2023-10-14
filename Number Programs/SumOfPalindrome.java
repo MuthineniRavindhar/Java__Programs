3ba2a02252bbcdcf37804f7be2aaaee79682bd5a
MuthineniRavindhar <139392018+MuthineniRavindhar@users.noreply.github.com>
2023-10-14 10:06:32 +0530

    Create SumOfPalindrome.java

---
+++ Number Programs/SumOfPalindrome.java
@@ -0,0 +1,29 @@
+package arrayprograms;
+
+public class SumOfPalindrome {
+	
+	public static void main(String[] args) {
+		
+		int a[]= {3,5,8,6,9,2,6,24};	
+		int sum=0;
+		for(int i=0; i<a.length; i++) {
+			sum=sum+a[i];
+			if(palin(sum)) {
+				System.out.println(sum+" is a polindrome");
+			}
+		}
+		
+		
+	}
+	
+	public static boolean palin(int num) {
+		int rev=0;
+		int temp=num;
+		while(num!=0) {
+			int rem=num%10;
+			rev=rev*10+rem;
+			num=num/10;
+		}
+		return rev==temp;
+	}
+}
